de67daec8951c183942d6bde89ebbed77decaaea
Ryan Hu <ryanhuuu@gmail.com>
2018-04-05 14:02:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,6 +6,10 @@ class School
   end
   
   def add_student(name, number)
-    
+    if @roster.has_key?(number)
+      @roster[number] << name
+    else
+      @roster[number] = [name]
+    end
   end
 end
